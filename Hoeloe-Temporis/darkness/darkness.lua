2b8127b4705179090c958286ecaae1ede034870d
Hoeloe <hoeloe@hotmail.co.uk>
2017-09-12 23:26:49 +0100

    Fix for a tiny mistake i made

--- Hoeloe-Temporis/darkness/darkness.lua
+++ Hoeloe-Temporis/darkness/darkness.lua
@@ -142,7 +142,7 @@ function Field:Draw()
 		};
 		]]
 		
-		capture1:captureAt(-1);
+		capture1:captureAt(-0.01);
 		
 		local cam = Camera.get()[1];
 		
@@ -187,7 +187,7 @@ function Field:Draw()
 			texture = capture1,
 			shader = self.shader,
 			uniforms = uniforms,
-			priority = -1
+			priority = -0.01
 			};
 	end
 end
