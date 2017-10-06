1fd810f6b4830c366acbf51379794a8ee6189b68
Hoeloe <hoeloe@hotmail.co.uk>
2017-10-06 11:24:19 +0100

    pause menu doesn't need workaround any more, and some minor temporis fixes

--- a2xt_pause.lua
+++ a2xt_pause.lua
@@ -332,12 +332,6 @@ function pause.Unblock()
 	pause.Blocked = false;
 end
 
-local lastinputs = {left = false, right = false};
-
-local function checkpressed(nm)
-	return player.keys[nm] and not lastinputs[nm];
-end
-
 function pause.onInputUpdate()
 	unregisterEvent(pm, "onInputUpdate", "onInputUpdate");
 	
@@ -376,19 +370,19 @@ function pause.onInputUpdate()
 		elseif(player.keys.up == KEY_PRESSED and confirm == nil) then
 			pause_option = (pause_option-1)%(#options);
 			Audio.playSFX(26)
-		elseif((checkpressed("left") or checkpressed("right"))) then
+		elseif(player.keys.left == KEY_PRESSED or player.keys.right == KEY_PRESSED) then
 			if(confirm ~= nil) then
 				confirm_option = 1-confirm_option;
 				Audio.playSFX(26)
 			elseif(isOverworld) then
-				if(checkpressed("left") and not player.keys.right) then
+				if(player.keys.left == KEY_PRESSED and not player.keys.right) then
 					currentChar = currentChar-1;
 					if(currentChar < 1) then
 						currentChar = 5;
 					end
 					leveldata.setCharacter(charList[currentChar]);
 					Audio.playSFX(26)
-				elseif(checkpressed("right") and not player.keys.left) then
+				elseif(player.keys.right == KEY_PRESSED and not player.keys.left) then
 					currentChar = currentChar+1;
 					if(currentChar > 5) then
 						currentChar = 1;
@@ -412,8 +406,6 @@ function pause.onInputUpdate()
 		end
 	end
 	presspause = player.keys.pause;
-	lastinputs.left = player.keys.left;
-	lastinputs.right = player.keys.right;
 end
 
 function pause.onTickEnd()
