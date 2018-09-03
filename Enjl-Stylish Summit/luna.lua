45df39a1b51602239fca776dfffe514b6f0c6e00
Enjl <enjlectric@gmail.com>
2018-09-03 17:19:12 +0200

    stylish summit is done

--- Enjl-Stylish Summit/luna.lua
+++ Enjl-Stylish Summit/luna.lua
@@ -6,20 +6,46 @@
 local particles = API.load("particles")
 local eventu = API.load("eventu")
 local a2xt_message = API.load("a2xt_message")
+local autoscroll = API.load("autoscroll")
 
 local e_snow = particles.Emitter(0,0,Misc.resolveFile("snowy.ini"))
 e_snow:AttachToCamera(Camera.get()[1])
 local urgh = Graphics.loadImage("urgh.png")
 
+local isScrolling = false
+
+local function startAutoscroll()
+	autoscroll.lockScreen()
+	eventu.waitFrames(250)
+	SFX.play(37)
+	Defines.earthquake = 3
+	local l = Layer.get("movewithautoscroll")
+	l.speedY = -1.25
+	autoscroll.scrollUp(1.25, nil, 2)
+	isScrolling = true
+end
+
 -- Run code on level start
 function onLoadSection()
     e_snow:setPrewarm(5)
+	if player.section == 2 then
+		eventu.run(startAutoscroll)
+	end
 end
+
+local cam = Camera.get()[1]
 
 -- Run code every frame (~1/65 second)
 -- (code will be executed before game logic will be processed)
 function onTick()
     --Your code here
+	if player:mem(0x122, FIELD_WORD) == 0 then
+		if isScrolling and cam.y < -163520 and cam.y == lastCamY then
+			local l = Layer.get("movewithautoscroll")
+			l.speedY = 0
+		end
+		lastCamY = cam.y
+	end
 end
 
 local showMessage = false
@@ -58,7 +84,9 @@ local cam = Camera.get()[1]
 -- Run code when internal event of the SMBX Engine has been triggered
 -- eventName - name of triggered event
 function onDraw()
-    e_snow:Draw(0)
+	if player.section == 0 or player.section == 3 then
+		e_snow:Draw(0)
+	end
 	if showMessage then
 		for k,v in ipairs(NPC.get(94, player.section)) do
 			Graphics.drawImageToSceneWP(urgh, v.x + - 45, v.y - 230, 5)
